8cd3b2791e50a0dd9e8904149288b0c198cb5457
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-05-22 20:39:57 +0300

    Create 01-even-sum.ts

---
+++ 01-intro/solutions/01-even-sum.ts
@@ -0,0 +1,5 @@
+function checkEvenSum(num1: number, num2: number, num3: number): boolean {
+    const sum: number = num1 + num2 + num3;
+    return sum % 2 === 0;
+}
+console.log(checkEvenSum(1, 2, 3)); 
