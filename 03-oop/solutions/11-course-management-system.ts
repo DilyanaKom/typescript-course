ceb42e18b309ed75c4805c028e7152fe3c4af431
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-06-05 11:28:42 +0300

    Create 11-course-management-system.ts

---
+++ 03-oop/solutions/11-course-management-system.ts
@@ -0,0 +1,46 @@
+abstract class Course {
+    title: string;
+    duration: number;
+
+    constructor(title: string, duration: number) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    abstract getDescription(): string;
+}
+
+class ProgrammingCourse extends Course {
+    language: string;
+
+    constructor(title: string, duration: number, language: string) {
+        super(title, duration);
+        this.language = language;
+    }
+
+    getDescription(): string {
+        return `Programming Course: ${this.title} in ${this.language} - ${this.duration} hour`
+    }
+}
+
+class DesignCourse extends Course {
+    tools: string[];
+
+    constructor(title: string, duration: number, tools: string[]) {
+        super(title, duration);
+        this.tools = tools;
+    }
+
+    getDescription(): string {
+        return `Design Course:: ${this.title} using ${this.tools} - ${this.duration} hour`
+    }
+}
+
+const jsCourse = new ProgrammingCourse("Intro to JavaScript", 40, "JavaScript");
+
+const uiCourse = new DesignCourse("UX Fundamentals", 30, ["Figma", "Sketch"]);
+
+
+console.log(jsCourse.getDescription());
+
+console.log(uiCourse.getDescription());
